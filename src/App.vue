ef477eb469c8668d178d3add4dbd595d1d535266
王一勤 <yiqin.wang@lilyenglish.com>
2019-09-08 18:07:48 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <imagef msg="vue zip file"/>
+    <audiof></audiof>
+  </div>
+</template>
+
+<script>
+import imagef from "./components/image";
+import audiof from "./components/audio";
+
+export default {
+  name: "app",
+  components: {
+    imagef,
+    audiof
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
